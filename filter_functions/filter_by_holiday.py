42f12cd03c42ba5b3f6b2eac7a66e29d7671cf79
mulfordjc <mulfordjc@appstate.edu>
2015-11-10 10:34:01 -0500

    Rewrote filter_by_holiday to handle larger csv files

--- filter_functions/filter_by_holiday.py
+++ filter_functions/filter_by_holiday.py
@@ -6,7 +6,7 @@ filter_by_holiday.py
 Takes a dataframe with converted dates and times, and filters the dataframe
 into categories based on the holidays
 """
-
+import pandas, numpy
 
 def labor_day(date):
     return date.month == 9 and (date.weekday() in [0,5,6]) and date.day < 7
@@ -34,24 +34,23 @@ def filter_data(data_frame):
     Takes a data frame created by convert_date_time.convert_csv_file and returns
     a dictionary filtered by the given holidays
     """
-    data_frame_dict = {}
-    for name in holiday_names:
-        data_frame_dict[name] = []
+    new_cols = numpy.append(data_frame.columns, 'filter')
+
+    new_data_frame = pandas.DataFrame([], columns=new_cols)
 
     for i in range(len(data_frame.values)):
+        print('Loop {}'.format(i))
         value = data_frame.values[i]
         date = data_frame['DATE'][i]
 
-        is_holiday = False
+        holiday_index = -1
 
         for j in range(len(holiday_functions)):
             func = holiday_functions[j]
             if func(date):
-                is_holiday = True
-                data_frame_dict[holiday_names[j]].append(value)
+                holiday_index = j                
                 break
+        data_frame_appender = pandas.DataFrame([numpy.append(value, holiday_names[holiday_index])], columns=new_cols)
+        new_data_frame.append(data_frame_appender, ignore_index=True)
 
-        if not is_holiday:
-            data_frame_dict[holiday_names[-1]].append(value)
-
-    return data_frame_dict
+    return new_data_frame
