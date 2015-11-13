147960f372eafa421c6951b8af1353bf632a4484
mulfordjc <mulfordjc@appstate.edu>
2015-11-13 13:07:14 -0500

    Updated generate_visuals to have column checking

--- generate_visuals.py
+++ generate_visuals.py
@@ -13,18 +13,25 @@ from convert_date_time import convert_csv_file
 
 def plot_incident_bar_graphs(csv_files):
     """
-    Takes csv files and creates bar graphs showing the incidents for each category in
+    Takes csv files and creates a scatter plot showing the incidents for each category in
     the csv files
-
-    Currently makes a scatter plot
     """
     count_list = []
+    max_count = 0
     for file in csv_files:
         data_frame = convert_csv_file(file)
+        if not 'COUNT' in data_frame.columns:
+            print('Error: the csv file must have a COUNT column')
+            exit(-1)
         count = 0
         for curr_count in data_frame['COUNT'].values:
             count += curr_count
         count_list.append(count)
-    
+        if count > max_count:
+            max_count = count
+
+    plt.ylim(0,max_count*1.5)
+    plt.xlim(-.5,len(count_list)-.5)
     plt.scatter(list(range(len(count_list))), count_list)
     plt.show()
+
